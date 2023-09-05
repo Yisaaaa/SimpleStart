98f99e9cc206f624635add0157e1fd93016a1f31
Yisaaaa <ryansanisit19@gmail.com>
2023-09-05 20:51:29 +0800

    added some basic config which includes the name, links, etc.

---
+++ config.js
@@ -0,0 +1,124 @@
+// BASIC CONFIG
+const CONFIG = {
+    // General
+    name: "Ryan",
+    twentyFourHourFormat: false,
+
+    // Greetings
+    greetingMorning: "Good morning, ",
+    greetingAfternoon: "Good afternoon, ",
+    greetingNight: "Good evening, ",
+
+    // Columns
+    firstColumnContainer: [
+        {
+            title: "Media",
+            contents: [
+                {
+                    id: "1",
+                    name: "Youtube",
+                    url: "https://www.youtube.com/",
+                },
+                {
+                    id: "2",
+                    name: "Spotify",
+                    url: "http://open.spotify.com/",
+                },
+                {
+                    id: "3",
+                    name: "YoutubeMusic",
+                    url: "https://music.youtube.com/",
+                },
+                {
+                    id: "4",
+                    name: "Netflix",
+                    url: "https://www.netflix.com/",
+                },
+            ],
+        },
+    ],
+
+    secondColumnContainer: [
+        {
+            title: "Social",
+            contents: [
+                {
+                    id: "1",
+                    name: "Facebook",
+                    url: "https://facebook.com/",
+                },
+                {
+                    id: "2",
+                    name: "Reddit",
+                    url: "https://reddit.com/",
+                },
+                {
+                    id: "3",
+                    name: "LinkedIn",
+                    url: "https://www.linkedin.com/",
+                },
+                {
+                    id: "4",
+                    name: "r/Philippines",
+                    url: "https://www.reddit.com/r/Philippines/",
+                },
+            ],
+        },
+    ],
+
+    thirdColumnContainer: [
+        {
+            title: "Study",
+            contents: [
+                {
+                    id: "1",
+                    name: "Github",
+                    url: "github.com/",
+                },
+                {
+                    id: "2",
+                    name: "MS Teams",
+                    url: "https://www.microsoft.com/en-us/microsoft-teams/",
+                },
+                {
+                    id: "3",
+                    name: "Notion",
+                    url: "https://www.notion.so/",
+                },
+                {
+                    id: "4",
+                    name: "Google Docs",
+                    url: "https://docs.google.com/",
+                },
+            ],
+        },
+    ],
+
+    fourthColumnContainer: [
+        {
+            title: "Others",
+            contents: [
+                {
+                    id: "1",
+                    name: "TheOdinProject",
+                    url: "https://www.theodinproject.com/paths",
+                },
+                {
+                    id: "2",
+                    name: "Wordtune",
+                    url: "https://www.wordtune.com/",
+                },
+                {
+                    id: "3",
+                    name: "ChatGPT",
+                    url: "https://chat.openai.com/",
+                },
+                {
+                    id: "4",
+                    name: "FullStackOpen",
+                    url: "https://fullstackopen.com/",
+                },
+            ],
+        },
+    ],
+};
