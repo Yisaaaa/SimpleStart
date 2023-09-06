eb4f24ddc7c81f09f2b533c37385bf4367d59223
Yisaaaa <ryansanisit19@gmail.com>
2023-09-06 14:06:14 +0800

    update

--- config.js
+++ config.js
@@ -23,115 +23,107 @@ const CONFIG = {
     greetingSleep: "Go to sleep, ",
 
     // Columns
-    firstColumnContainer: [
-        {
-            title: "Media",
-            contents: [
-                {
-                    id: "1",
-                    name: "Youtube",
-                    url: "https://www.youtube.com/",
-                },
-                {
-                    id: "2",
-                    name: "Spotify",
-                    url: "http://open.spotify.com/",
-                },
-                {
-                    id: "3",
-                    name: "YoutubeMusic",
-                    url: "https://music.youtube.com/",
-                },
-                {
-                    id: "4",
-                    name: "Netflix",
-                    url: "https://www.netflix.com/",
-                },
-            ],
-        },
-    ],
+    firstColumnContainer: {
+        title: "Media",
+        contents: [
+            {
+                id: "1",
+                name: "Youtube",
+                url: "https://www.youtube.com/",
+            },
+            {
+                id: "2",
+                name: "Spotify",
+                url: "http://open.spotify.com/",
+            },
+            {
+                id: "3",
+                name: "YoutubeMusic",
+                url: "https://music.youtube.com/",
+            },
+            {
+                id: "4",
+                name: "Netflix",
+                url: "https://www.netflix.com/",
+            },
+        ],
+    },
 
-    secondColumnContainer: [
-        {
-            title: "Social",
-            contents: [
-                {
-                    id: "1",
-                    name: "Facebook",
-                    url: "https://facebook.com/",
-                },
-                {
-                    id: "2",
-                    name: "Reddit",
-                    url: "https://reddit.com/",
-                },
-                {
-                    id: "3",
-                    name: "LinkedIn",
-                    url: "https://www.linkedin.com/",
-                },
-                {
-                    id: "4",
-                    name: "r/Philippines",
-                    url: "https://www.reddit.com/r/Philippines/",
-                },
-            ],
-        },
-    ],
+    secondColumnContainer: {
+        title: "Social",
+        contents: [
+            {
+                id: "1",
+                name: "Facebook",
+                url: "https://facebook.com/",
+            },
+            {
+                id: "2",
+                name: "Reddit",
+                url: "https://reddit.com/",
+            },
+            {
+                id: "3",
+                name: "LinkedIn",
+                url: "https://www.linkedin.com/",
+            },
+            {
+                id: "4",
+                name: "r/Philippines",
+                url: "https://www.reddit.com/r/Philippines/",
+            },
+        ],
+    },
 
-    thirdColumnContainer: [
-        {
-            title: "Study",
-            contents: [
-                {
-                    id: "1",
-                    name: "Github",
-                    url: "github.com/",
-                },
-                {
-                    id: "2",
-                    name: "MS Teams",
-                    url: "https://www.microsoft.com/en-us/microsoft-teams/",
-                },
-                {
-                    id: "3",
-                    name: "Notion",
-                    url: "https://www.notion.so/",
-                },
-                {
-                    id: "4",
-                    name: "Google Docs",
-                    url: "https://docs.google.com/",
-                },
-            ],
-        },
-    ],
+    thirdColumnContainer: {
+        title: "Study",
+        contents: [
+            {
+                id: "1",
+                name: "Github",
+                url: "github.com/",
+            },
+            {
+                id: "2",
+                name: "MS Teams",
+                url: "https://www.microsoft.com/en-us/microsoft-teams/",
+            },
+            {
+                id: "3",
+                name: "Notion",
+                url: "https://www.notion.so/",
+            },
+            {
+                id: "4",
+                name: "Google Docs",
+                url: "https://docs.google.com/",
+            },
+        ],
+    },
 
-    fourthColumnContainer: [
-        {
-            title: "Others",
-            contents: [
-                {
-                    id: "1",
-                    name: "TheOdinProject",
-                    url: "https://www.theodinproject.com/paths",
-                },
-                {
-                    id: "2",
-                    name: "Wordtune",
-                    url: "https://www.wordtune.com/",
-                },
-                {
-                    id: "3",
-                    name: "ChatGPT",
-                    url: "https://chat.openai.com/",
-                },
-                {
-                    id: "4",
-                    name: "FullStackOpen",
-                    url: "https://fullstackopen.com/",
-                },
-            ],
-        },
-    ],
+    fourthColumnContainer: {
+        title: "Others",
+        contents: [
+            {
+                id: "1",
+                name: "TheOdinProject",
+                url: "https://www.theodinproject.com/paths",
+            },
+            {
+                id: "2",
+                name: "Wordtune",
+                url: "https://www.wordtune.com/",
+            },
+            {
+                id: "3",
+                name: "ChatGPT",
+                url: "https://chat.openai.com/",
+            },
+            {
+                id: "4",
+                name: "FullStackOpen",
+                url: "https://fullstackopen.com/",
+            },
+        ],
+    },
 };
